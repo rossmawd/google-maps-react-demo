864c38e83fde28d85e69db6dfc38adc3fce58713
Ross Mawdsley <rossmawd@gmail.com>
2019-08-04 14:51:14 +0100

    Learning git with udacity

--- src/App.js
+++ src/App.js
@@ -74,9 +74,7 @@ export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_GOOGLE_KEY
-        }`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD97EWwT9HMl18Apu-zK9PP20QhYEccvo4`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
